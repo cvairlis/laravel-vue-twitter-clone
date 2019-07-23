7e06eb073598ab58274e6c8fffb470e7eb3913b5
greekit <h.vairlis@gmail.com>
2019-07-24 02:55:03 +0300

    Modify app nav bar.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-7">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header d-flex flex-row pb-0" style="border:none;">
+                    <div class="p-2"><strong>username</strong> - <a href="/profile">link</a> to the profile page - DateTime of tweet</div>
+                </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="card-body d-flex flex-row pt-0">
+                    <div class="p-2">Tweet text</div>
                 </div>
             </div>
         </div>
