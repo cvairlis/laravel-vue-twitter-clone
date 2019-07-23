3fe532fc983297d67c9ce3da6ad556186cfc01b6
greekit <h.vairlis@gmail.com>
2019-07-23 04:19:12 +0300

    Simple timeline view and routing.

---
+++ resources/views/timeline.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Timeline</div>
+                    <div class="card-body">
+                        @if (Auth::check())
+                            <div class="alert alert-success" role="alert">
+                                You are logged in!
+                            </div>
+
+                        @else
+                            You have to <a href="{{ route('login') }}">login</a> in order to continue.
+                        @endauth
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', ['middleware' =>'guest', function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/timeline', function (){
+    return view('timeline');
+});
+
