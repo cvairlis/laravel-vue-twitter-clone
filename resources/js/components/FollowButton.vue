534d6674c8d7f84c494b50ef70bac7aded332c83
greekit <h.vairlis@gmail.com>
2019-07-24 14:56:23 +0300

    Vue JS For follow / unfollow user profile.

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
-        <button class="btn btn-primary ml-4">Follow</button>
+        <button class="btn btn-primary ml-4" @click="followUser" v-text="buttonText">Follow</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: "FollowButton"
+        props: ['userId','follows'],
+
+        data: function() {
+            return {
+                status: this.follows,
+            }
+        },
+
+        methods: {
+            followUser() {
+                axios.post('/follow/'+this.userId)
+                    .then(response => {
+                        this.status=!this.status;
+                        console.log(response.data);
+                    })
+            }
+        },
+
+        computed: {
+            buttonText() {
+                return (this.status) ? 'Unfollow' : 'Follow';
+            }
+        }
     }
 </script>
 
