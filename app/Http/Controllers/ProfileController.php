9cc9b99b444641d83128d8ae9702c6156980f78d
greekit <h.vairlis@gmail.com>
2019-07-24 04:28:59 +0300

    This is the correct way to accept a username and return the whole DB user found with this username.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use http\Client\Curl\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class ProfileController extends Controller
 {
-    public function index()
+    public function index($username)
     {
+        $userFromDatabase = DB::table('Users')->where('username', '=', $username)->first();
         return view('profile',[
-            'user' => Auth::user()
+            'user' => $userFromDatabase
         ]);
     }
 }
