94cd3cc170c2938aeda9ceac9dcef5fb9296a9a3
greekit <h.vairlis@gmail.com>
2019-07-24 21:07:16 +0300

    Created index.blade.php within resources/posts to put inside the view of the timeline!

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-7">
+                <h3><strong>Timeline</strong></h3>
+                @forelse ($posts as $post)
+                    <div class="card mb-3">
+                        <div class="card-header d-flex flex-row pb-0" style="border:none;">
+                            <div class="p-2">At {{ $post->created_at->format('d-m-Y H:i:s') }}, user <strong><a href="/profile/{{ $post->user->username }}">{{ $post->user->username }}</a></strong> posted the following tweet:</div>
+                        </div>
+
+                        <div class="card-body d-flex flex-row pt-0">
+                            <div class="p-3 pl-4">{{ $post->body }}</div>
+                        </div>
+                    </div>
+                @empty
+                    <p>No tweet</p>
+                @endforelse
+
+            </div>
+        </div>
+    </div>
+@endsection
