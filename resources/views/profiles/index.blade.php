c16bbb4bddb632f22344f85b89cd3e8070676167
greekit <h.vairlis@gmail.com>
2019-07-24 14:56:51 +0300

    Added follow / unfollow button on user profile page.

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,7 @@
                     <div class="card-body">
                         <div class="d-flex pb-3">
                             <h3 class="pt-2">Username: <strong>{{ $user->username }}</strong></h3>
-                            <button class="btn btn-primary ml-4">Follow</button>
+                            <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
                         </div>
                         <div>Email: <strong>{{ $user->email }}</strong></div>
                         <div class="row h-100 justify-content-center align-items-center">
