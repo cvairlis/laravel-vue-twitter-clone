d9fb78ed3063702c1ed284c798a69ac7a1a36261
greekit <h.vairlis@gmail.com>
2019-07-24 21:05:46 +0300

    Timeline finally made within the PostsController!!

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 class PostsController extends Controller
 {
 
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $users = auth()->user()->following()->pluck('profiles.user_id');
+        $posts = Post::whereIn('user_id', $users)->latest()->get();
+        return view('posts.index', compact('posts'));
     }
 
     public function show()
