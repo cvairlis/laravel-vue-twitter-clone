b7310c4857c4cd2bc2895910bdafb229e602d638
greekit <h.vairlis@gmail.com>
2019-07-24 07:29:06 +0300

    Some changes to home blade.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-7">
-            @foreach($user->posts as $post)
-                <div class="card mb-3">
-                    <div class="card-header d-flex flex-row pb-0" style="border:none;">
-                        <div class="p-2"><strong>username</strong> - <a href="/profile">link</a> to the profile page - DateTime of tweet</div>
-                    </div>
-
-                    <div class="card-body d-flex flex-row pt-0">
-                        <div class="p-2">Tweet text</div>
-                    </div>
+            <div class="card mb-3">
+                <div class="card-header d-flex flex-row pb-0" style="border:none;">
+                    <div class="p-2"><strong>username</strong> - <a href="/profile">link</a> to the profile page - DateTime of tweet</div>
                 </div>
-            @endforeach
 
+                <div class="card-body d-flex flex-row pt-0">
+                    <div class="p-2">Tweet text</div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
