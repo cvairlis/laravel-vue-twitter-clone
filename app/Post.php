bd2aa747cf1cce373d368d589a937a0b64a8f5e1
greekit <h.vairlis@gmail.com>
2019-07-24 17:36:38 +0300

    First attempt for the Timeline!

--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,10 @@ class Post extends Model
 {
     protected $guarded = [];
 
+    protected $fillable = [
+        'user_id', 'body',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,21 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-7">
-            <div class="card mb-3">
-                <div class="card-header d-flex flex-row pb-0" style="border:none;">
-                    <div class="p-2"><strong>username</strong> - <a href="/profile">link</a> to the profile page - DateTime of tweet</div>
-                </div>
+            <h3><strong>Timeline</strong></h3>
+            @forelse ($user->timeline() as $post)
+                <div class="card mb-3">
+                    <div class="card-header d-flex flex-row pb-0" style="border:none;">
+                        <div class="p-2"><strong></strong> - <a href="/profile">link</a> to the profile page - DateTime of tweet</div>
+                    </div>
 
-                <div class="card-body d-flex flex-row pt-0">
-                    <div class="p-2">Tweet text</div>
+                    <div class="card-body d-flex flex-row pt-0">
+                        <div class="p-2">{{ $post->body }}</div>
+                    </div>
                 </div>
-            </div>
+            @empty
+                <p>No tweet</p>
+            @endforelse
+
         </div>
     </div>
 </div>
