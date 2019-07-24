fb72cda9252e3cac2b17133fdb1aad8daa5a7cde
greekit <h.vairlis@gmail.com>
2019-07-24 21:07:58 +0300

    Removed contents from home.blade.php. No in use.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-7">
-            <h3><strong>Timeline</strong></h3>
-            @forelse ($user->timeline() as $post)
-                <div class="card mb-3">
-                    <div class="card-header d-flex flex-row pb-0" style="border:none;">
-                        <div class="p-2"><strong></strong> - <a href="/profile">link</a> to the profile page - DateTime of tweet</div>
-                    </div>
-
-                    <div class="card-body d-flex flex-row pt-0">
-                        <div class="p-2">{{ $post->body }}</div>
-                    </div>
-                </div>
-            @empty
-                <p>No tweet</p>
-            @endforelse
-
+            empty
         </div>
     </div>
 </div>
