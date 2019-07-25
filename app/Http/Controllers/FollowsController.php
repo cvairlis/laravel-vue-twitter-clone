6e0969d1ea5fb1b6699fbd06806e7028b3c07685
greekit <h.vairlis@gmail.com>
2019-07-26 00:47:56 +0300

    Small change on brackers. While searching how to implement the new follower email notification system!

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\UserFollowed;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Log;
 
 class FollowsController extends Controller
 {
@@ -12,7 +16,8 @@ class FollowsController extends Controller
         $this->middleware('auth');
     }
 
-    public function store(User $user){
+    public function store(User $user)
+    {
         return auth()->user()->following()->toggle($user->profile);
     }
 }
