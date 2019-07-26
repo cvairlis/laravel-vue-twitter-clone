ae4ad328f25bc1dd5771e6a38013106d111f5b63
greekit <h.vairlis@gmail.com>
2019-07-26 18:20:31 +0300

    Renamed UserListsController to UsersListsController!

--- app/Http/Controllers/UsersListsController.php
+++ app/Http/Controllers/UsersListsController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 
-class UsersListController extends Controller
+class UsersListsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $users = User::where('id', '!=', auth()->id())->paginate(5);
