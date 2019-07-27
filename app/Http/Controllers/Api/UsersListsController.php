91bc19797b874bc42092904bffaeab461a5230e8
greekit <h.vairlis@gmail.com>
2019-07-28 02:21:40 +0300

    Moved back the hidden User Model attributes in their initial state and added columns at the UsersListsController index function!

--- app/Http/Controllers/Api/UsersListsController.php
+++ app/Http/Controllers/Api/UsersListsController.php
@@ -10,7 +10,15 @@ class UsersListsController extends BaseController
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::all(
+            'username',
+            'email',
+            'total_followers',
+            'total_following',
+            'total_tweets_posted',
+            'link_to_profile',
+            'link_to_avatar'
+            );
 
         return $this->sendResponse($users->toArray(), 'Users retrieved successfully.');
     }
