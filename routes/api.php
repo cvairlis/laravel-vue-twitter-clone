85cbeb9c457c3285db2807c16e0eca9e8ef6354d
greekit <h.vairlis@gmail.com>
2019-07-28 02:31:42 +0300

    Created api route for the statistics!

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::get('/users', 'Api\UsersListsController@index');
 
+Route::get('/statistics', 'Api\StatisticsController@index');
+
