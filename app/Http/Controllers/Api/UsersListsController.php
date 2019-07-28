d7be527afc6d4b64e86fab3265ed392795fcac4f
greekit <h.vairlis@gmail.com>
2019-07-28 04:56:54 +0300

    Changed procedure to retreive data from database.

--- app/Http/Controllers/Api/UsersListsController.php
+++ app/Http/Controllers/Api/UsersListsController.php
@@ -3,22 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\User;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class UsersListsController extends BaseController
 {
     public function index()
     {
-        $users = User::all(
-            'username',
-            'email',
-            'total_followers',
-            'total_following',
-            'total_tweets_posted',
-            'link_to_profile',
-            'link_to_avatar'
-            );
+        $users = DB::table('users')->select( 'username',
+                'email',
+                'total_followers',
+                'total_following',
+                'total_tweets_posted',
+                'link_to_profile',
+                'link_to_avatar')->get();
 
         return $this->sendResponse($users->toArray(), 'Users retrieved successfully.');
     }
